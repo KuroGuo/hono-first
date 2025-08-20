49e7adb99a78c575e9033747ce038666389aed4e
Kuro <kuroguo@outlook.com>
2025-08-21 01:37:21 +0800

    build: 更新构建配置并删除旧的入口文件
    - 在 .gitignore 文件中添加 dist/index.js
    - 删除 dist 目录下的旧入口文件 index.js

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-import { serve } from '@hono/node-server';
-import app from './app/index.js';
-const PORT = +(process.env.PORT || 3000);
-serve({ fetch: app.fetch, port: PORT }, info => {
-    console.log(`Server running on http://localhost:${info.port}`);
-});
